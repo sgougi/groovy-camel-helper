b1e595a0a7f7d5fe7478fa740b9285caba8d7b8b
Shigeru GOUGI <sgougi@gmail.com>
2015-04-12 16:41:18 +0900

    departed from the method of using a timer component.

--- README.md
+++ README.md
@@ -3,23 +3,20 @@
 
 ```
 @GrabResolver(name=':groovy-camel-helper', root='http://www.wingnest.com/mvn-repo/')
-@Grab('com.wingnest.groovy:groovy-camel-helper:1.0-SNAPSHOT')
+@Grab('com.wingnest.groovy:groovy-camel-helper:1.0-M1-SNAPSHOT')
 
-import static com.wingnest.groovy.camel.CamelHelper.newStopProcessor as stop
-import static com.wingnest.groovy.camel.CamelHelper.getOnceTimerUri as once
+import static com.wingnest.groovy.camel.CamelHelper.getDirectOnceUri as once
 import static com.wingnest.groovy.camel.CamelHelper.startCamelContext
 
 def camelContext = new DefaultCamelContext()
 camelContext.addRoutes(new RouteBuilder() {
     def void configure() {
-        onException(Exception.class).process(stop(-1))
         from(once())
         // components, processor, ....
-        .process(stop(0))
     }
 })
 
-startCamelContext(this, camelContext)
+startCamelContext(camelContext)
 ```
 
 Licence
--- src/main/java/com/wingnest/groovy/camel/CamelHelper.java
+++ src/main/java/com/wingnest/groovy/camel/CamelHelper.java
@@ -1,55 +1,21 @@
 package com.wingnest.groovy.camel;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultExchange;
 
 public final class CamelHelper {
 
-    public static Processor newStopProcessor(int status) {
-        return new StopProcessor(status);
-    }
-    
-    public static Processor newStopProcessor() {
-        return new StopProcessor(0);
-    }    
-
-    public static String getOnceTimerUri() {
-        return "timer:?fixedRate=true&repeatCount=1";
+    public static String getDirectOnceUri() {
+        return "direct:once";
     }
 
-    public static void startCamelContext(Object script, CamelContext camelContext) throws Exception {
-        camelContext.start();
-        synchronized(script){
-           script.wait();
-        }
-    }
-
-    public static class StopProcessor implements Processor {
-        private Thread stop;
-        private int status;
-
-        public StopProcessor(int status) {
-            this.status = status;
-        }
-
-        public void process(final Exchange exchange) throws Exception {
-            if (stop == null) {
-                stop = new Thread() {
-                    public void run() {
-                        try {
-                            exchange.getContext().stop();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        System.exit(status);
-                    }
-                };
-            }
-            stop.start();
-        }
+    public static void startCamelContext(CamelContext camelContext) throws Exception {
+    	camelContext.start();
+    	camelContext.createProducerTemplate().send("direct:once", new DefaultExchange(camelContext));
+    	camelContext.stop();
     }
 
     private CamelHelper() { /* noop */ }
 
 }
+
